06a3373b9c0a589c099a885b251fc5bab44a112f
sachin-yadavv <ysachin120504@gmail.com>
2026-05-09 23:37:32 +0530

    added hero section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import Hero from "../components/Home/Hero"
+
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <Hero />
+      
     </div>
   )
 }
