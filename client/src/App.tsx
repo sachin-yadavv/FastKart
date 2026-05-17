c6122345447a0e09b527466099cb458101500aa5
sachin-yadavv <ysachin120504@gmail.com>
2026-05-17 21:28:19 +0530

    added the admin and delivery partner dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,79 @@
-import { Toaster } from 'react-hot-toast'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import AppLayout from './pages/AppLayout'
-import  Home  from './pages/Home'
-import Products from './pages/Products'
-import ProductPage from './pages/ProductPage'
-import SearchResults from './pages/SearchResults'
-import FlashDeals from './pages/FlashDeals'
-import Checkout from './pages/Checkout'
-import MyOrders from './pages/MyOrders'
-import OrderTracking from './pages/OrderTracking'
-import Addresses from './pages/Addresses'
-import ProtectedRoute from './components/ProtectedRoute'
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductPage from "./pages/ProductPage";
+import SearchResults from "./pages/SearchResults";
+import FlashDeals from "./pages/FlashDeals";
+import Checkout from "./pages/Checkout";
+import MyOrders from "./pages/MyOrders";
+import OrderTracking from "./pages/OrderTracking";
+import Addresses from "./pages/Addresses";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminDeliveryPartners from "./pages/admin/AdminDeliveryPartners";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminProductForm from "./pages/admin/AdminProductForm";
+import AdminProducts from "./pages/admin/AdminProducts";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminLayout from "./pages/admin/AdminLayout";
+import DeliveryLogin from "./pages/delivery/DeliveryLogin";
+import DeliveryLayout from "./pages/delivery/DeliveryLayout";
+import DeliveryDashboard from "./pages/delivery/DeliveryDashboard";
 
 const App = () => {
   return (
     <>
-     <Toaster position='top-right' toastOptions={{duration : 3000, style:{
-      background: '#1b3022', color : '#fff', fontSize : '14px', borderRadius : '12px'
-     }}}/> 
-     <Routes>
-      {/* auth pages - no navbar/footer */}
-      <Route path='/login' element={<Login />} />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#1b3022",
+            color: "#fff",
+            fontSize: "14px",
+            borderRadius: "12px",
+          },
+        }}
+      />
+      <Routes>
+        {/* auth pages - no navbar/footer */}
+        <Route path="/login" element={<Login />} />
 
-      {/* main - pages - with navbar/footer */}
-      <Route path='/' element={<AppLayout />} >
-         <Route index element ={<Home />} />
-         <Route path="products"element ={<Products />} />
-         <Route path="products/:id"element ={<ProductPage />} />
-         <Route path="search"element ={<SearchResults />} />
-         <Route path="deals"element ={<FlashDeals />} />
-         <Route element={<ProtectedRoute />} >
-          <Route path='checkout' element={<Checkout />}/>
-          <Route path='orders' element={<MyOrders />}/>
-          <Route path='orders/:id' element={<OrderTracking />}/>
-          <Route path='addresses' element={<Addresses />}/>
-            
+        {/* main - pages - with navbar/footer */}
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<ProductPage />} />
+          <Route path="search" element={<SearchResults />} />
+          <Route path="deals" element={<FlashDeals />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<MyOrders />} />
+            <Route path="orders/:id" element={<OrderTracking />} />
+            <Route path="addresses" element={<Addresses />} />
           </Route>
-         </Route>
-      
-      </Routes> 
-    </>
-  )
-}
+        </Route>
 
-export default App
+        {/* admin pages */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="products/new" element={<AdminProductForm />} />
+          <Route path="products/: id/edit" element={<AdminProductForm />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="delivery-partners" element={<AdminDeliveryPartners />} />
+        </Route>
+
+        {/* delivery partner pages */}
+        <Route path="/delivery/login" element={<DeliveryLogin />} />
+        <Route path="/delivery" element={<DeliveryLayout />}>
+          <Route index element={<DeliveryDashboard />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
