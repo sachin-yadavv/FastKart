653fc7b5bd76f29dbd4a55897397b27c0c1627e9
sachin-yadavv <ysachin120504@gmail.com>
2026-06-01 09:27:15 +0530

    added the stripe keys and online payment option

--- server/controllers/orderController.ts
+++ server/controllers/orderController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
 import { inngest } from "../inngest/index.js";
+import Stripe from "stripe";
 
 // Create order
 // POST /api/orders
@@ -90,6 +91,29 @@ export const createOrder = async (req: Request, res: Response) => {
 
   if (paymentMethod === "card") {
     // stripe payment link
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+    // create session
+    // create session
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${req.headers.origin}/orders?clearCart=true`,
+      cancel_url: `${req.headers.origin}/checkout`,
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Payment Groceries",
+            },
+            unit_amount: Math.round(total * 100),
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      metadata: { orderId: order.id },
+    });
+    return res.json({ url: session.url });
   }
 
   res.json({ order });
